a5af88ea5d1a9f0e85c6517301e747366656f143
Ivan Verdugo <ivan270@gmail.com>
2023-04-12 13:46:33 -0400

    se agrega el modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,4 +28,37 @@ $(function () {
 	containerFecha.text(fecha);
 	numeroVuelo.text(generator(1, 9999));
 	terminal.text(generator(1, 10));
+	// Modal
+
+	$('body').append(`
+    <div
+    class="modal fade in"
+    id="aeroModal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="aeroModalLabel"
+>
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button
+                        type="button"
+                        class="close"
+                        data-dismiss="modal"
+                        aria-label="Close"
+                    >
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">Pasajero, se le recuerda que para abordar el avión debe tener en mano su pasaporte y su boleto de embarque. Manténgase atento a los avisos mediante esta plataforma.</div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">
+                        Cerrar
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    `);
+	$('#aeroModal').modal('show');
 });
